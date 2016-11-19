7327e4980a134d8498443f7242d18b4f8a6fb814
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2016-11-20 00:54:17 +0200

    List Projects as a <table> element.

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
 
-require_once 'config.php';
-
-DataFile::Load();
-
-Project::LoadProjects();
+require_once 'initialize.php';
 
 echo "Count projects: ".count(Project::All())."<br>";
 
 
 /** @var Project $project */
+echo '<table><thead><tr>'.Project::Dummy()->HTML()->many('th',true).'</tr></thead><tbody>';
 foreach (Project::All() as $project)
 {
-	echo $project->name;
+	echo '<tr>'.$project->HTML()->many('td').'</tr>';
 }
+echo '</tbody></table>';
