1a20c35a5d3b7bc3e7b01543aa613a10599ab92c
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2016-11-20 04:41:50 +0200

    index.php: Remove Project number counter as it's not really important.

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 require_once 'code/initialize.php';
 
-echo "Count projects: ".count(Project::All())."<br>";
-
-
 /** @var Project $project */
 echo '<table><thead><tr>'.Project::Dummy()->HTML()->many('th',true).'</tr></thead><tbody>';
 foreach (Project::All() as $project)
